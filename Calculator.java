a62aa0f76ac693016933273fc437710f46063ef9
niketajain123 <niketajain236@gmail.com>
2025-01-07 10:58:53 +0530

    Subtraction of 5-5

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,5 @@ class Calculator{
  public static void main(String[]args){
 	System.out.println("Welcome to calculator!");
 	System.out.println("5+5 : "+(5+5));
+	System.out.println("5-5 : "+(5-5));
 }}
